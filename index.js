cb0b48a96932cc96e2982bbe6936d64b8b596602
Margaret <wanjirundirangu43@gmail.com>
2022-06-09 15:33:08 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+function receivesAFunction(callback){
+    callback();
+}
+function returnsANamedFunction(){
+    return returnsANamedFunction;
+}
+function returnsAnAnonymousFunction(){
+    return (function(){});
+}
